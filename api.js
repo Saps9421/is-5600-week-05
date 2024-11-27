1cb1006374559ab905846369748b7dcce8193e48
Saps9421 <nyalakondasahithya@gmail.com>
2024-11-27 05:58:56 +0000

    week 05

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
+@@ -1,57 +1,17 @@
 const path = require('path')
 const Products = require('./products')
+const Orders = require('./orders')
 const autoCatch = require('./lib/auto-catch')
 
 /**
@@ -47,34 +49,56 @@ async function getProduct(req, res, next) {
 /**
  * Create a product
  * @param {object} req 
+@@ -50,36 +51,48 @@ async function getProduct(req, res, next) {
  * @param {object} res 
  */
 async function createProduct(req, res) {
   console.log('request body:', req.body)
   res.json(req.body)
+  const product = await Products.create(req.body)
+  res.json(product)
 }
 
 /**
- * Edit a product
- * @param {object} req
- * @param {object} res
- * @param {function} next
- */
+@@ -63,6 +23,10 @@ async function createProduct(req, res) {
 async function editProduct(req, res, next) {
   console.log(req.body)
   res.json(req.body)
+  const change = req.body
+  const product = await Products.edit(req.params.id, change)
+
+  res.json(product)
 }
 
 /**
- * Delete a product
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
+@@ -73,13 +37,38 @@ async function editProduct(req, res, next) {
  */
 async function deleteProduct(req, res, next) {
   res.json({ success: true })
+async function deleteProduct (req, res, next) {
+  const response = await Products.destroy(req.params.id)
+
+  res.json(response)
 }
 
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+  res.json(order)
+}
+
+
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({ 
+    offset: Number(offset), 
+    limit: Number(limit),
+    productId, 
+    status 
+  })
+
+  res.json(orders)
+}
 module.exports = autoCatch({
   handleRoot,
   listProducts,
@@ -82,4 +106,8 @@ module.exports = autoCatch({
   createProduct,
   editProduct,
   deleteProduct
+});
+  deleteProduct,
+  listOrders,
+  createOrder
 });
